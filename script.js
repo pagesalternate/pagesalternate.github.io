de145d754d4b06c5afb6545e6a2df569ebaa7906
pagesalternate <75673814+pagesalternate@users.noreply.github.com>
2020-12-08 18:11:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const	body = document.querySelector('body')
+const twitter = document.querySelector('.twitter')
+
+twitter.addEventListener("mouseover", function () {
+    body.classList.add('linked')
+},false)
+
+twitter.addEventListener("mouseout", function () {
+    body.classList.remove('linked')
+}, false)
+
+function myFunction() {
+//   document.getElementById("b1").style.color = "red";
+
+}
