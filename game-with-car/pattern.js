45a101f0aa6c1dadaa3c31089d25c6fdf3ab803c
pagesalternate <75673814+pagesalternate@users.noreply.github.com>
2021-02-03 06:23:22 +0530

    Update pattern.js

--- game-with-car/pattern.js
+++ game-with-car/pattern.js
@@ -4,14 +4,14 @@ var patterns = [
     "dots normal",
     "polka normal",
     "lines horizontal",
-    "lines vertical",
-    "lines plus",
-    "lines cross",
-    "lines left",
-    "lines right",
-    "dots heavy",
-    "triangles",
-    "planes",
+//    "lines vertical",
+//    "lines plus",
+//    "lines cross",
+//    "lines left",
+//    "lines right",
+//    "dots heavy",
+//    "triangles",
+//    "planes",
 ];
 
 var temp = [...patterns];
